d4dece91686b6a36a7a57ff278f8d5263ae8e9e2
ptwinny <105453320+ptwinny@users.noreply.github.com>
2023-01-13 11:32:04 -0600

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -3,20 +3,23 @@ const elems = ["title-change-color-six", "title-change-color-five", "title-chang
 const colors = [["--main-red-orange", "#ff5e00"], ["--main-darkgreen", "#318c11"], ["--main-red", "#ff0000"],
   ["--main-purple", "#6F0B8D"], ["--main-pink", "#ff00c8"], ["--main-blue", "#002aff"]];
 let elemCount = 0;
-let colorCount = 1;
-window.addEventListener("load", setInterval, false);
+let colorCount = 0;
 
-setInterval(changeColors, 500);
-/* fix!!!!!!!!!!!!!!!!!!!!!!!!!!!
-function changeColors() {
-  while (count < 6) {
-    document.getElementById(elems[elemCount]).style.stroke = colors[colorCount][1];
-    elemCount++;
-    colorCount++;
-    if (elemCount === 5) {
-      elemCount = 0;
-      colorCount = 1;
-    }
+/* not working bc doesn't change after one round
+function changeColor() {
+  document.getElementById(elems[elemCount]).style.stroke = colors[colorCount][1];
+  elemCount++;
+  colorCount++;
+  
+  if (elemCount === elems.length) {
+    elemCount = 0;
   }
+  if (colorCount === colors.length) {
+    colorCount = 0;
+  }
+  console.log("elemCount: " + elemCount);
+  console.log("colorCount: " + colorCount);
 }
 */
+
+window.onload = setInterval(changeColor, 500);
